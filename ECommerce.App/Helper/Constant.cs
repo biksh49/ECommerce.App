c2985d7389425a1557e6ca8ef2383ce53a4fb750
Anchal <Anchal@DESKTOP-33OJO2E>
2023-10-16 04:28:49 -0700

    Making SQl server to SQl Authentication #

--- ECommerce.App/Helper/Constant.cs
+++ ECommerce.App/Helper/Constant.cs
@@ -2,7 +2,7 @@
 {
     public class Constant
     {
-        public static string ConnectionString_MSSQL = "Server=desktop-33ojo2e\\sqlexpress;Database=dbECommerce;Trusted_Connection=True;MultipleActiveResultSets=true;Encrypt=True;TrustServerCertificate=True";
+        public static string ConnectionString_MSSQL = "Server=desktop-33ojo2e\\sqlexpress;Database=dbECommerce;Password=98121;Trusted_Connection=False;MultipleActiveResultSets=true;Encrypt=True;TrustServerCertificate=True";
         //public static string ConnectionString_MySQL = "server=root@localhost:3306;database=Ecommerce;Password=root@1234\"";
         // WE can implement multiple  Database by this process
 
