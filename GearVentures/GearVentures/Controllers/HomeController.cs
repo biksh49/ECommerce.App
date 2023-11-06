bc724e5e9946a849f341c9107745cb9f454c25e1
Pranab Shrestha <80814582+pranabshrestha4@users.noreply.github.com>
2023-11-06 11:12:25 +0545

    E-commerce Dev project

---
+++ GearVentures/GearVentures/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using GearVentures.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace GearVentures.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About() 
+        { 
+            return View();
+        }
+
+        public IActionResult Shop() 
+        { 
+            return View();
+        }
+
+        public IActionResult Contact() 
+        { 
+            return View();
+        }
+    }
+}
